c33b8fed6ed6da84a89b8a6124bf1bdf0c239c6b
1594218302@qq.com <1594218302@qq.com>
2020-05-20 16:37:58 +0800

    1

--- src/components/tabar/HomeContainer.vue
+++ src/components/tabar/HomeContainer.vue
@@ -253,9 +253,10 @@
     },
 
     methods: {
+      //跳转小程序主页面
       goMini() {
         window.wx.miniProgram.navigateTo({
-          url: 'pages/payMent/payMent',
+          url: '/pages/payMent/payMent',
           success: function () {
             console.log('success')
           },
@@ -272,7 +273,7 @@
       configInfo() {
         // 通过config接口注入权限验证配置 【必需】
         // alert(this.miniInfo.appId)
-        window.wx.config({
+        wx.config({
           debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: this.miniInfo.appId, // 必填，公众号的唯一标识
           timestamp: this.miniInfo.timestamp, // 必填，生成签名的时间戳
