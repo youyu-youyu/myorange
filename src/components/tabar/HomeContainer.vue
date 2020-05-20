27fc7e5356d1127277e37eafe0d9112b1c448cbf
1594218302@qq.com <1594218302@qq.com>
2020-05-20 17:28:59 +0800

    1

--- src/components/tabar/HomeContainer.vue
+++ src/components/tabar/HomeContainer.vue
@@ -275,14 +275,14 @@
       },
       configInfo() {
         // 通过config接口注入权限验证配置 【必需】
-        // alert(this.miniInfo.appId)
+        alert(this.miniInfo.signature)
         wx.config({
           debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: this.miniInfo.appId, // 必填，公众号的唯一标识
           timestamp: this.miniInfo.timestamp, // 必填，生成签名的时间戳
           nonceStr: this.miniInfo.nonceStr, // 必填，生成签名的随机串
           signature: this.miniInfo.signature,// 必填，签名
-          jsApiList: ["checkJsApi", "scanQRCode"],// 必填，需要使用的JS接口列表 这里填写需要用到的微信api openlocation为使用微信内置地图查看位置接口
+          jsApiList: ["chooseWXPay", "scanQRCode"],// 必填，需要使用的JS接口列表 这里填写需要用到的微信api openlocation为使用微信内置地图查看位置接口
         });
         window.wx.error(function (res) {
         });
