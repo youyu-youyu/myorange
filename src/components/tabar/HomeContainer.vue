01fcf763295eefc380923b1b02c749b79d5374f8
1594218302@qq.com <1594218302@qq.com>
2020-05-20 17:25:06 +0800

    1

--- src/components/tabar/HomeContainer.vue
+++ src/components/tabar/HomeContainer.vue
@@ -287,21 +287,12 @@
         window.wx.error(function (res) {
         });
         window.wx.ready(function () {
-          alert("配置成功")
-          window.wx.checkJsApi({
-            jsApiList: ['checkJsApi', 'openLocation', 'getNetworkType'],
+          wx.scanQRCode({
+            needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+            scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
             success: function (res) {
-
-            }
-          });
-          window.wx.getNetworkType({
-            success: function (res) {
-              var networkType = res.networkType; // 返回网络类型2g，3g，4g，wifi
-              console.log(networkType)
-            }, fail: function (res) {
-              console.log("getNetworkType failed")
-            }, complete: function (res) {
-              console.log("complete")
+              var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+              alert("scan code:" + result)
             }
           });
         });
