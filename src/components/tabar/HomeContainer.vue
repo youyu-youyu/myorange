2364bfbe76c1dd5319329a80de06a6165f8baddb
1594218302@qq.com <1594218302@qq.com>
2020-05-20 15:46:27 +0800

    1

--- src/components/tabar/HomeContainer.vue
+++ src/components/tabar/HomeContainer.vue
@@ -270,6 +270,7 @@
       },
       configInfo() {
         // 通过config接口注入权限验证配置 【必需】
+        alert(this.miniInfo.appId)
         window.wx.config({
           debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: this.miniInfo.appId, // 必填，公众号的唯一标识
@@ -320,11 +321,10 @@
 
           .then(res => {
             console.log("获取jssdk")
-            console.log(res.body.data)
             if (res.body.err_code === 0) {
               this.miniInfo = res.body.data
               this.configInfo()
-              console.log(this.miniInfo)
+
             } else
               alert("获取微信jssdk配置失败:" + res.body.message)
           })
