221aea7f99ba1d39dfae12bbcd37383023cabc1d
1594218302@qq.com <1594218302@qq.com>
2020-05-21 17:46:30 +0800

    1

--- src/components/tabar/HomeContainer.vue
+++ src/components/tabar/HomeContainer.vue
@@ -269,7 +269,7 @@
 
       },
       configJSSDK(miniInfo) {
-        alert("配置jssdk")
+        alert("配置jssdk" + wx)
         // 通过config接口注入权限验证配置 【必需】
         wx.config({
           debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
