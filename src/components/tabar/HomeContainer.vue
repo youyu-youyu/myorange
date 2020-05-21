69dc5e02dd10c9b43a50046040bb01a04c57a096
1594218302@qq.com <1594218302@qq.com>
2020-05-21 10:16:24 +0800

    1

--- src/components/tabar/HomeContainer.vue
+++ src/components/tabar/HomeContainer.vue
@@ -455,16 +455,17 @@
              */
             //如果存在以AE开头的测试二维码
             if (result.startsWith("AE") && result.length === 12) {
+              alert(result)
               _this.deviceCode = result.substring(4, 12)
 
               // 00：机器，01：售币机
               if (result.substring(2, 4) === "01") {
-                _this.window.document.getElementById("cover").setAttribute("style", "display:block;")
-                _this.window.document.getElementById("selectPay_id").setAttribute("style", "display:block;")
+                _this.document.getElementById("cover").setAttribute("style", "display:block;")
+                _this.document.getElementById("selectPay_id").setAttribute("style", "display:block;")
                 //
               } else if (result.substring(2, 4) === "00") {
-                _this.window.document.getElementById("cover").setAttribute("style", "display:block;")
-                _this.window.document.getElementById("robot_id").setAttribute("style", "display:block;")
+                _this.document.getElementById("cover").setAttribute("style", "display:block;")
+                _this.document.getElementById("robot_id").setAttribute("style", "display:block;")
               }
             }
 
