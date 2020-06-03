e3853366df6c57a03403c3e2a66a3fd4f8fb100e
戚美英 <1594218302@qq.com>
2020-06-03 11:03:45 +0800

    1

--- src/components/tabar/HomeContainer.vue
+++ src/components/tabar/HomeContainer.vue
@@ -171,6 +171,7 @@
       };
     },
     created: function () {
+      this.parseUrlBrand();
       if (this.type === 1) {
         if (global_msg.company !== -1) {
           this.getCode();
@@ -243,7 +244,7 @@
     mounted() {
 
       // console.log("AE010055023".substring(4, 12))
-      this.parseUrlBrand();
+
 
 
     },
