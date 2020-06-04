d23ee26ee97aa668d622bfeb16d0358b9069b092
戚美英 <1594218302@qq.com>
2020-06-04 11:10:53 +0800

    1

--- src/components/tabar/HomeContainer.vue
+++ src/components/tabar/HomeContainer.vue
@@ -177,13 +177,14 @@
       if (this.type === 1) {
         if (global_msg.company !== -1) {
           //如果第一次进来或者token'过期才授权，其他进来，不用跳到授权页面
-          if (!localStorage.getItem("isTokenExpire", "false") && this.code == null || this.code === "") {
-            this.getCode();
-          } else {
-            // 授完权才有code
-            this.publicAccountLogin();
-          }
-
+          // if (!localStorage.getItem("isTokenExpire", "false") && this.code == null || this.code === "") {
+          this.getCode();
+          // } else {
+          // 授完权才有code
+          this.publicAccountLogin();
+          // }
+          //每次进首页，先到首页，然后到跳转微信授权，再从微信授权完之后重定向回首页
+          //现在要做的就是：如果第一次进来或者token'过期才授权，其他时候进来，不用跳到授权页面直接登录
         } else {
           this.getLastSelectedShop();
         }
@@ -390,7 +391,7 @@
             }
           });
       },
-      //公众哈登录
+      //公众号登录
       publicAccountLogin() {
         this.$http
           //定义为全局使用global_msg.server_url
@@ -402,8 +403,8 @@
               // 固定值type：1:公众号，2:小程序
             }, {emulateJSON: true})
           .then(res => {
-            alert("this.code")
-            alert(this.code)
+            // alert("this.code")
+            // alert(this.code)//undefined
             if (res.body.err_code === 0) {
               localStorage.setItem('token_type', res.body.data.token_type);
               localStorage.setItem('token', res.body.data.access_token);
