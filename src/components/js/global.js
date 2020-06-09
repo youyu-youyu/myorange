5f83be3d59cf628dbaa0d5d382184aa274c4d9a5
戚美英 <1594218302@qq.com>
2020-06-09 13:26:23 +0800

    1

--- src/components/js/global.js
+++ src/components/js/global.js
@@ -171,7 +171,8 @@ export default {
 
         } else {
           console.log("111getLocation")
-            `${this.getLocation(_this)}`;
+          this.getLocation(_this)
+          // `${this.getLocation(_this)}`;
           // _this.getLocation(_this);
         }
 
