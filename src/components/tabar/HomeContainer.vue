94ed9fa3b6f964a603218f3078448d19e1f026ee
戚美英 <1594218302@qq.com>
2020-06-19 10:11:21 +0800

    1

--- src/components/tabar/HomeContainer.vue
+++ src/components/tabar/HomeContainer.vue
@@ -452,10 +452,11 @@
           needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
           scanType: ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
           success: function (res) {
-            var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+            let result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
             if (result.indexOf("qrStorageTicket") !== -1) {
               result = JSON.parse(result)
             }
+            alert(result)
             /**
              * 扫到存彩票的二维码
              */
