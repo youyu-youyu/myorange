d0c9d62dba953fe30856f845679d05f079fb3f39
戚美英 <1594218302@qq.com>
2020-06-28 14:30:28 +0800

    1

--- src/components/tabar/HomeContainer.vue
+++ src/components/tabar/HomeContainer.vue
@@ -472,6 +472,8 @@
           needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
           scanType: ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
           success: function (res) {
+            Toast("jing入success")
+            alert("jing入success alert")
             let scanResult = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
             if (scanResult.indexOf("qrStorageTicket") !== -1 || scanResult.indexOf("catering_table") !== -1) {
               scanResult = JSON.parse(scanResult)
